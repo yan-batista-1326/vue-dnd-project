b218bd506451f9ace5d9ec49f784a58326913168
Yan Batista <ybatistasouza@hotmail.com>
2020-12-05 17:46:41 -0300

    Build inicial do projeto

---
+++ src/components/UserCrud.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-container>
+        <v-row class="text-center">
+            <v-col cols="12">
+                <h1>User Crud</h1>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+export default {
+    name: "UserCrud",
+    data() => ({
+        users: [
+            {id: 1, name: 'José', location: 'Rio de Janeiro'},
+            {id: 2, name: 'Fulano', location: 'São Paulo'},
+        ]
+    });
+}
